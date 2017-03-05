9c7360070b89b228dc7d7327f3d14e403ef2b768
Alex Omelchenko <a.n.omelchenko@icloud.com>
2017-03-05 08:59:53 +0200

    fixed conversable strings handling

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = (value) => {
     const num = parseInt(value, 10);
-    if ((num || value === 0) && !Array.isArray(value)) {
+    if ((num || num === 0) && !Array.isArray(value)) {
         return Boolean(num % 2);
     }
     throw new Error('Value is not a number');
--- test/index.js
+++ test/index.js
@@ -3,13 +3,19 @@ const isValueOdd = require('..');
 
 describe('Basic check', function() {
     it('should check Even properly', function() {
-        assert.equal(isValueOdd(2), false, 'Fail');
+        assert.equal(isValueOdd(0), false, 'Fail');
     });
     it('should check Odd properly', function() {
         assert.equal(isValueOdd(1), true, 'Fail');
     });
     it('should handle Zero properly', function() {
         assert.equal(isValueOdd(0), false, 'Fail');
+    });
+    it('should handle conversable String properly', function() {
+        assert.equal(isValueOdd('1'), true, 'Fail');
+    });
+    it('should handle nonconversable String properly', function() {
+        assert.throws(() => isValueOdd('hello'), Error, 'Fail');
     });
     it('should handle Object properly', function() {
         assert.throws(() => isValueOdd({a: 'hello'}), Error, 'Fail');
