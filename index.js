98e3203db20578a695bda810e38ddcf342f593b2
Alex Omelchenko <a.n.omelchenko@icloud.com>
2017-03-11 21:33:17 +0200

    isNaN used

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = (value) => {
     const num = parseInt(value, 10);
-    if ((num || num === 0) && !Array.isArray(value)) {
+    if (!isNaN(num) && !Array.isArray(value)) {
         return Boolean(num % 2);
     }
     throw new Error('Value is not a number');
